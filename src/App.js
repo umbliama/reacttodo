51d4c729501fd72b1849ea7b7e81507a39a470a6
Maxim Umblia <umblia34@gmail.com>
2019-08-08 21:43:14 +0700

    add store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
-import TodoStore from "./store/TodoStore";
-
-function App() {
-  return <div className="App" />;
+import React, { Component } from "react";
+import TodoList from "./components/TodoList";
+import { observer } from "mobx-react";
+import AddTodo from "./components/AddTodo";
+@observer
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    const store = this.props.store;
+    return (
+      <div>
+        <AddTodo store={store} />
+        <TodoList store={store} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/store/TodoStore.js
+++ src/store/TodoStore.js
@@ -1,12 +1,37 @@
 import { observable } from "mobx";
 import { computed } from "mobx";
 
-class TodoList {
-  @observable todos = [];
+class TodoStore {
+  @observable todos = [
+    {
+      id: 1,
+      name: "buy milk",
+      isCompleted: false
+    },
+    {
+      id: 2,
+      name: "take a walk with dog",
+      isCompleted: true
+    }
+  ];
 
-  @computed get todos() {
+  @observable filter = "";
+  @computed get getTodos() {
     return this.todos;
+  }
+
+  addTodo(task) {
+    this.todos.push({
+      id: this.todos.length + 1,
+      name: task,
+      isCompleted: false
+    });
+    console.log("====================================");
+    console.log("pushed");
+    console.log("====================================");
   }
 }
 
-export default TodoList;
+const store = new TodoStore();
+
+export default store;
