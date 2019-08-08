26bc347fccd586b3a0511205727b6be114f77ff5
Maxim Umblia <umblia34@gmail.com>
2019-08-08 22:53:01 +0700

    disable add button

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -17,23 +17,12 @@ class AddTodo extends Component {
   addTodo = () => {
     this.props.store.addTodo(this.state.textTodo);
   };
-  buttonDisabled = () => {
-    const { textTodo } = this.state;
-    if (textTodo === "") {
-      console.log("false");
 
-      return false;
-    } else {
-      console.log("true");
-
-      return true;
-    }
-  };
   render() {
     return (
       <div>
         <input type="text" onChange={this.setTask} />
-        <button disabled={Boolean(this.buttonDisabled)} onClick={this.addTodo}>
+        <button disabled={!this.state.textTodo} onClick={this.addTodo}>
           Add
         </button>
       </div>
